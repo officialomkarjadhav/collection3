9091f53014d1356139e22e08ec2fc79540981aaa
omkar jadhav <officialomkarjadhav@gmail.com>
2023-03-11 10:36:32 +0000

    task10 completed

---
+++ task10.java
@@ -0,0 +1,12 @@
+import java.util.HashMap;
+
+public class task10 {
+    public static void main(String[] args) {
+        HashMap <Integer,String> hm= new HashMap<>();
+        hm.put(1,"red");
+         hm.put(2,"blue");
+          hm.put(3,"green");
+         hm.put(4,"yellow");
+         System.out.println("collection view is "+hm.values());
+    }
+}
